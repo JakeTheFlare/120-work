950fdcb04f3ebb972d15f01e8dad537c2e5e2237
Jacob Williams <jacob2.williams@umconnect.umt.edu>
2017-09-23 22:19:30 -0600

    Week 3 Homework
    This is my code sketch for my week 3 homework.

---
+++ hw-3/sketch.js
@@ -0,0 +1,16 @@
+// creating the setup function
+function setup() {
+
+  // creating a canvas 550px wide and 400px high
+  createCanvas(550, 400)
+
+  // coloring the background 'red'
+  background('red')
+}
+
+// creating the drawing function
+function draw() {
+  // tell p5 to print the the text string "Hello World!"
+  // Positioning this text string at (x:30, y:30).
+  text("Hello World!", 30, 30)
+}
